a10183d63965d511164340e47ac377a42e4de96a
Scotturbina <kevin.urbinamoreira@hotmail.com>
2017-01-17 20:10:19 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function is going to create a matrix as an object that can cache its inverse value
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        Invalid<- NULL
+        Setting<- function(z) {
+                x<<-z
+                Invalid<<- NULL
 
 }
+
+        Obt<- funcion() x
+        SetInv<- function(inv) invs <<-inv
+        ObtInv<- function() invs
+          list(Setting=Setting,
+               Obt=Obt,  SetInv= SetInv
+               ,ObtInv=ObtInv)
+                
+ }
+                
+                     
+
 
 
 ## Write a short comment describing this function
